9d9d4fa1764844064987fe9bdabfbd19e905995e
lizan <442926536@qq.com>
2017-12-18 20:32:22 +0800

    angular4同步

--- ng4-quickstart/src/app/app.component.ts
+++ ng4-quickstart/src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  // template: `<h1>Hello {{name}}</h1>`,
-  templateUrl: 'app/1.html',
+  template: `
+  <a routerLink="/">首页</a>
+  <a routerLink="/user">用户</a>
+  <router-outlet></router-outlet>
+  `,
+  // templateUrl: 'app/1.html',
 })
 export class AppComponent  { name = 'jquery'; }
+
