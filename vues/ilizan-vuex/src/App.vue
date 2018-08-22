1d684f47a336ac715117b9403c65be793fb98c1c
lizan <442926536@qq.com>
2018-08-22 18:11:43 +0800

    添加vuex

---
+++ vues/ilizan-vuex/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <b-button>按钮</b-button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+</style>
