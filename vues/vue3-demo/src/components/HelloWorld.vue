3a20cc84908c2ef0b29e989711fdc751dc39c9be
lizan <442926536@qq.com>
2018-08-24 18:53:47 +0800

    同步

---
+++ vues/vue3-demo/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>helloworld-{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    //方法
+    fn() {
+      console.log("fn");
+    }
+  },
+  mounted() {
+    //页面加载完
+    console.log(this.msg);
+    this.fn();
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vues/vue3-demo/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="msgInfo"/>
+
+    <HelloWorld v-bind:msg="name" />
+    <input type="text" v-model="name">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      name: "homeName",
+      list: [
+        {
+          id: 1,
+          name: "name1"
+        },
+        {
+          id: 2,
+          name: "name2"
+        },
+        {
+          id: 3,
+          name: "name3"
+        }
+      ]
+    };
+  }
+};
+</script>
