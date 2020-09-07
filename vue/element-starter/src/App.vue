d88dccfbcdd152499c45fb66de1f047ced1839d4
lizan <lizan@shengbangtec.com>
2020-09-07 15:32:29 +0800

    增加vue

---
+++ vue/element-starter/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <el-button @click.native="startHacking">弹窗提示</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Use Vue 2.0 Today!'
+    }
+  },
+
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: '提示',
+        message: '文字',
+        duration: 6000
+      })
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ vue/vue-admin/src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+	<section>
+		mainpage
+	</section>
+</template>
+
+<script>
+	export default {
+	}
+
+</script>
+
+<style scoped>
+
+</style>
